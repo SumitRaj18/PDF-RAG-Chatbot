13f539b62e7956f577953a48edd6ed269edc06af
SumitRaj18 <sumitraj7162@gmail.com>
2026-09-02 17:52:49 +0530

    changes

--- server/src/config/passport.js
+++ server/src/config/passport.js
@@ -6,7 +6,7 @@ import User from '../models/User.js';
 passport.use(new GoogleStrategy({
   clientID: process.env.GOOGLE_CLIENT_ID,
   clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  callbackURL: '/api/auth/google/callback'
+  callbackURL: 'https://pdf-rag-chatbot-0le9.onrender.com/api/auth/google/callback'
 }, async (accessToken, refreshToken, profile, done) => {
   try {
     let user = await User.findOne({ googleId: profile.id });
